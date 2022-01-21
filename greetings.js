dadea4d68ebc07c47a301823ecf72d6e02385108
❖✯Duleyaboy✯❖ <97851042+DULEYABOY@users.noreply.github.com>
2022-01-21 23:59:21 +0530

    Create greetings.js

---
+++ greetings.js
@@ -0,0 +1,24 @@
+
+const PluginDB = Build.DATABASE.define('Plugin', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    url: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+});
+
+async function installPlugin(adres, file) {
+    var Plugin = await PluginDB.findAll({
+        where: {url: adres}
+    });
+
+    if (Plugin.length >= 1) {
+        return false;
+    } else {
+        return await PluginDB.create({ url: adres, name: file });
+    }
+}
+module.exports = { PluginDB: PluginDB, installPlugin: installPlugin }
